b342913bb7a9e90731aa3f3518175b91a0552cde
Zed <clement.francois@epitech.eu>
2018-01-27 23:37:15 +0100

    main: CHANGE: SceneObject construction according to its new namespace and tag-constructor

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <cassert>
 
 int main() {
-	Scene::SceneObject obj;
+	constexpr std::size_t sphereTag = 1;
+	Entity::SceneObject obj{ sphereTag };
 	obj.add_component<Component::Position>();
 	auto& base = obj.get_component<Component::Position::idxPosition>();
 	auto position = static_cast<Component::Position&>(base);
