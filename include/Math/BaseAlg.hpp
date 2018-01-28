05b5f999c43a61944917292c7ac91536aba8e5bc
Zed <clement.francois@epitech.eu>
2018-01-28 22:58:18 +0100

    BaseAlg: ADD: First implementation of base alg

---
+++ include/Math/BaseAlg.hpp
@@ -0,0 +1,22 @@
+#ifndef BASE_ALG_HPP__
+#define BASE_ALG_HPP__
+
+namespace Math {
+
+template<typename Derived>
+class BaseAlg {
+public:
+	auto size() const { return derived().size(); }
+	auto col() const { return derived().row(); }
+	auto row() const { return derived().col(); }
+	
+	auto& operator()(const std::size_t height, const std::size_t width) { return derived()(height, width); }
+	const auto& operator()(const std::size_t height, const std::size_t width) { return derived()(height, width); }
+private:
+	auto &derived() { return static_cast<Derived&>(*this); };
+	auto const &derived() const { return static_cast<Derived const&>(*this); };
+};
+
+} // namespace Math
+
+#endif
