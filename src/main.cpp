5d6a34141d906ce455dd2857ae79bc15e05c732f
Zed <clement.francois@epitech.eu>
2018-01-28 22:12:46 +0100

    main: CHANGE: Index name

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
 	constexpr std::size_t sphereTag = 1;
 	Entity::SceneObject obj{ sphereTag };
 	obj.add_component<Component::Position>();
-	auto& base = obj.get_component<Component::Position::idxPosition>();
+	auto& base = obj.get_component<Component::Position::idx>();
 	auto position = static_cast<Component::Position&>(base);
 	assert(position.x == 0.f && position.y == 0.f && position.z == 0.f);
 }
