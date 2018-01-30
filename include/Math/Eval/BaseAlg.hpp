81f1513318d9ace6d7f9c1172ea63a0ef70e3a8e
Zed <clement.francois@epitech.eu>
2018-01-30 22:00:40 +0100

    BaseAlg: RENAME: width and height by X and Y

--- include/Math/Eval/BaseAlg.hpp
+++ include/Math/Eval/BaseAlg.hpp
@@ -10,12 +10,23 @@ public:
 	auto col() const { return derived().row(); }
 	auto row() const { return derived().col(); }
 	
-	auto& operator()(const std::size_t height, const std::size_t width) { return derived()(height, width); }
-	const auto& operator()(const std::size_t height, const std::size_t width) { return derived()(height, width); }
+	auto& operator()(const std::size_t x, const std::size_t y) { return derived()(x, y); }
+	const auto& operator()(const std::size_t x, const std::size_t y) const { return derived()(x, y); }
 private:
 	auto& derived() { return static_cast<Derived&>(*this); };
 	auto const& derived() const { return static_cast<Derived const&>(*this); };
 };
+
+template<typename D1>
+void eval(BaseAlg<D1>& out, const BaseAlg<D1>& alg1) {
+	auto col = out.col();
+	auto row = out.row();
+	for (std::size_t i = 0; i < row; ++i) {
+		for (std::size_t j = 0; j < col; ++j) {
+			out(i, j) = alg1(i, j);
+		}
+	}
+}
 
 } // namespace Math
 
