b6c141a1e80f75edaa7b0d87cfee125ddf64794e
Zed <clement.francois@epitech.eu>
2018-01-30 22:33:08 +0100

    Matrix: FIX: Compile errors and evaluation during operations

--- include/Math/Matrix.hpp
+++ include/Math/Matrix.hpp
@@ -19,22 +19,32 @@ class Matrix : public BaseAlg<Matrix<T, WIDTH, HEIGHT>> {
 
 public:
 	Matrix() = default;
-	template<typename... Args>
-	Matrix(Args&&... args) : data_{ { std::forward<Args>(args)... } } {};
+
+	Matrix(std::initializer_list<T> init) {
+		assert(init.size() == Size::value && "Size in the brace MUST be the same as the template argument");
+		auto it = init.begin();
+		for (std::size_t i = 0; i < row(); ++i) {
+			for (std::size_t j = 0; j < col(); ++j) {
+				data_[i * WIDTH + j] = *it;
+				++it;
+			}
+		}
+	}
+	
 	template<typename D>
 	Matrix(const BaseAlg<D>& alg) {
 		assert(alg.row() == row() && alg.col() == col() && "Error, both column and row number must be the same");
 		eval(*this, alg);
 	}
-
+	
 	auto& operator()(const std::size_t x, const std::size_t y) {
-		assert(height * X + width > Size::value && "Index out of range");
-		return data_[y * X + x];
+		assert(y * WIDTH + x < Size::value && "Index out of range");
+		return data_[y * WIDTH + x];
 	};
 
 	const T& operator()(const std::size_t x, const std::size_t y) const {
-		assert(height * X + width > Size::value && "Index out of range");
-		return data_[y * X + x];
+		assert(y * WIDTH + x < Size::value && "Index out of range");
+		return data_[y * WIDTH + x];
 	};
 
 	auto row() const { return Row::value; }
@@ -46,17 +56,17 @@ private:
 
 template<typename T, std::size_t W, std::size_t H>
 auto operator+(const Matrix<T, W, H>& lhs, const Matrix<T, W, H>& rhs) {
-	return Matrix<T, W, H>{ Addition<Matrix<T, W, H>, Matrix<T, W, H>>{ *this, rhs } }; 
+	return Addition<Matrix<T, W, H>, Matrix<T, W, H>>{ lhs, rhs }; 
 }
 
 template<typename T, std::size_t W, std::size_t H>
 auto operator-(const Matrix<T, W, H>& lhs, const Matrix<T, W, H>& rhs) { 
-	return Matrix<T, W, H>{ Substraction<Matrix<T, W, H>, Matrix<T, W, H>>{ *this, rhs } }; 
+	return Substraction<Matrix<T, W, H>, Matrix<T, W, H>>{ lhs, rhs }; 
 }
 
 template<typename T, std::size_t W, std::size_t H>
 auto operator*(const Matrix<T, W, H>& lhs, const Matrix<T, W, H>& rhs) {
-	return Matrix<T, W, H>{ Multiplication<Matrix<T, W, H>, Matrix<T, W, H>>{ *this, rhs } };
+	return Matrix<T, W, H>{ Multiplication<Matrix<T, W, H>, Matrix<T, W, H>>{ lhs, rhs } };
 }
 
 } // namespace Math
