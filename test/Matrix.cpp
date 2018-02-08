95e7f3a514ddc77f83f3dd12bcf7a01db003ed71
Clement Francois <cfrancois@localhost.localdomain>
2018-02-08 15:21:46 -0500

    Matrix: ADD: Row and column test

--- test/Matrix.cpp
+++ test/Matrix.cpp
@@ -11,18 +11,18 @@ SCENARIO("We can instanciate a simple matrix") {
 	}
 }
 
-SCENARIO("Matrices are array where we can store as [row x col]") {
-	//Math::Matrix<float, 
-}
-
 SCENARIO("We can add 2 matrices") {
 	GIVEN("2 2x2 float matrices") {
 		Math::Matrix<float, 2, 2> m{ 1.f, 1.f, 1.f, 1.f  };
 		Math::Matrix<float, 2, 2> m2{ 1.f, 1.f, 1.f, 1.f };
+
 		auto result = m + m2;
 
 		auto col = result.col();
 		auto row = result.row();
+
+		REQUIRE(col == 2);
+		REQUIRE(row == 2);
 
 		for (std::size_t i = 0; i < row; ++i) {
 			for (std::size_t j = 0; j < col; ++j) {
