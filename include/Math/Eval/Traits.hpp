5a1abc430ce1457b78e6709caef7c955a1b66400
Clement Francois <cfrancois@localhost.localdomain>
2018-02-08 15:20:16 -0500

    Traits: REVERSE: value_row and value_col + ADD: assert_same_type

--- include/Math/Eval/Traits.hpp
+++ include/Math/Eval/Traits.hpp
@@ -10,12 +10,12 @@ template<typename T> struct traits;
 template<
 	template<class, std::size_t, std::size_t> class M, 
 	typename T, 
-	std::size_t W, 
-	std::size_t H
-> struct traits<M<T, W, H>> {
+	std::size_t H, 
+	std::size_t W
+> struct traits<M<T, H, W>> {
 	using type = T;
-	using width = std::integral_constant<std::size_t, W>;
-	using height = std::integral_constant<std::size_t, H>;
+	using value_col = std::integral_constant<std::size_t, W>;
+	using value_row = std::integral_constant<std::size_t, H>;
 };
 
 template<typename D1, typename D2> struct common_type {
@@ -28,11 +28,21 @@ template<
 	class D2
 > struct traits<Eval<D1, D2>> {
 	using type = typename common_type<D1, D2>::type;
+	using value_col = typename traits<D1>::value_col;
+	using value_row = typename traits<D2>::value_row;
 };
 
 template<template<class> typename V, typename T> struct traits<V<T>> {
 	using type = T;
+	using value_col = std::integral_constant<std::size_t, 1>;
+	using value_row = std::integral_constant<std::size_t, 3>;
 };
+
+template<typename T> struct type_wrapper {};
+
+template<class T1, class T2> inline void assert_same_type(T1, T2) {
+	type_wrapper<T1>{} = type_wrapper<T2>{};
+}
 
 } // namespace Math
 
