8be99cc9c0c7132bd112d2c921bbc185303212fc
Clement Francois <cfrancois@localhost.localdomain>
2018-02-09 11:34:15 -0500

    main: ADD: First main for non-test binary

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include "Shapes/SceneObject.hpp"
-#include "Components/Position.hpp"
+#include "include/Shapes/SceneObject.hpp"
+#include "include/Components/Position.hpp"
 #include <cassert>
+#include <iostream>
 
 int main() {
 	constexpr std::size_t sphereTag = 1;
@@ -8,5 +9,6 @@ int main() {
 	obj.add_component<Component::Position>();
 	auto& base = obj.get_component<Component::Position::idx>();
 	auto position = static_cast<Component::Position&>(base);
+	std::cout << "eclair a la vaniiiiiiiiille" << std::endl;
 	assert(position.x == 0.f && position.y == 0.f && position.z == 0.f);
 }
